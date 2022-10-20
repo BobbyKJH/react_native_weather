d0c15707a0b79f57ddad8387ffbfc34d2e6b777b
BobbyKJH <rlatnguscjsw@gmail.com>
2022-10-20 16:05:43 +0900

    Feat: React-Navite Create

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>HI</Text>
+      <StatusBar style="black" />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#000",
+  },
+});
